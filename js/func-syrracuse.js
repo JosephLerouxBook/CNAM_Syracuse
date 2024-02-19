4ff675ae04521fb4e54cf5651d4a67c747a3b40f
Joseph Leroux <41457887+JosephLerouxBook@users.noreply.github.com>
2024-02-19 16:55:13 +0100

    FlexBox + Résultat
    CSS : flexbox added, radius...
    Tableau résultat added, Suite en dessous.
    
    Todo :
    Gerer l'input isNaN.

--- js/func-syrracuse.js
+++ js/func-syrracuse.js
@@ -1,25 +1,50 @@
-function syrracuse(nbr){
-    var keepNbr = nbr;
+//variable global, pas la meilleure solution mais ca marche.
+var wasitdonebefore = false;
+
+function syrracuse(){
+    //annule le reload de la page du boutton submit
+    event.preventDefault();
+    var keepNbr = document.getElementById("nbr").value;
+    //stock les valeurs generer par l'algorithme
     var resArr = Array();
     var i = 0;
-    while(nbr != 1){
-        resArr[i] = nbr;
+    // Verifie l'entrée de l'utilisateur.
 
-        if(nbr%2 == 0){
-            nbr = nbr / 2;
+        //check, sinon possibilité de réappuyer sur le boutton en boucle et de regenerer le tableau en boucle.
+        if (wasitdonebefore == false) {
+            while(keepNbr != 1){
+                resArr[i] = keepNbr;
+
+                if(keepNbr%2 == 0){
+                    keepNbr = keepNbr / 2;
+                }
+                else if(keepNbr%2 == 1){
+                    keepNbr = (keepNbr*3) + 1;
+                }
+                i++;
+                /* DEBUG
+                console.log(resArr);
+                console.log(i);
+                console.log("---------------");
+                */
+            }
+            i++;
+            resArr[i] = keepNbr;
+            resArr[i+1] = 1;
+            /* DEBUG
+            console.log(keepNbr);
+            console.log(i);
+            console.log("---------------");
+            */
+            //Envoie les nombres de la suite générer au générateur de tableau
+            handleResArr(resArr);
+            //change la variable global pour affirmer que la suite a deja été generer.
+            wasitdonebefore = true;
         }
-        else if(nbr%2 == 1){
-            nbr = (nbr*3) + 1;
+        else{
+            alert("Recharger la page pour essayer un autre nombre.");
         }
-        i++;
-        console.log(resArr);
-        console.log(i);
-        console.log("---------------");
-    }
-    i++;
-    resArr[i+1] = 1;
-    console.log(nbr);
-    console.log(i);
-    console.log("---------------");
-    document.getElementById("resultat").innerHTML = is;
+
+     
 }
+
---
+++ js/func-tableauResultat.js
@@ -0,0 +1,44 @@
+// fonction de generation du  tableau de résultat.
+function handleResArr(resArr){
+    event.preventDefault();
+    var i = 0;
+    const table = document.getElementById("resTable");
+    const tbody = document.getElementById("targetbodyRes");
+    document.getElementById("suite").innerHTML += "{";
+    
+    while (resArr[i]){
+        const row = document.createElement("tr");
+        /* DEBUG
+        console.log("EH");
+        console.log(i);
+        */
+        const cellValue = document.createElement("td");
+        const cellIndex = document.createElement("td");
+        const cellVText = document.createTextNode(resArr[i]);
+        const cellIText = document.createTextNode(i + 1);
+        cellValue.appendChild(cellVText);
+        cellIndex.appendChild(cellIText);
+        row.appendChild(cellIndex);
+        row.appendChild(cellValue);
+        tbody.appendChild(row);
+        table.appendChild(tbody)
+
+        document.getElementById("suite").innerHTML +=  resArr[i] + ", ";
+        i++;
+    }
+    //derniere ligne du tableau.
+    const row = document.createElement("tr");
+    const cellValue = document.createElement("td");
+    const cellIndex = document.createElement("td");
+    const cellVText = document.createTextNode(resArr[i + 1]);
+    const cellIText = document.createTextNode(i + 1);
+    cellValue.appendChild(cellVText);
+    cellIndex.appendChild(cellIText);
+    row.appendChild(cellIndex);
+    row.appendChild(cellValue);
+    tbody.appendChild(row);
+    table.appendChild(tbody)
+
+    document.getElementById("suite").innerHTML += "1}";
+   console.log(resArr);
+}
