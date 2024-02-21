7a30fd343e6b2fcaf5a92736be4805682a62985d
Joseph Leroux <41457887+JosephLerouxBook@users.noreply.github.com>
2024-02-21 11:49:25 +0100

    Font changed
    added :
    - Folder 'font'
    - Changed font of <p> and <h1>
    - CSS for fixing width and heigh
    - CSS for cat page
    
    Todo :
    - Verify input data from user

--- js/func-tableauResultat.js
+++ js/func-tableauResultat.js
@@ -40,6 +40,6 @@ function handleResArr(resArr){
     table.appendChild(tbody)
 
     document.getElementById("suite").innerHTML += "1}";
-    document.getElementById("Ev'rybody Wants to Be a Cat").innerHTML = "<a href=\"cat.html\">Vous avez bien meritez un chat.</a>";
+    document.getElementById("Ev'rybody Wants to Be a Cat").innerHTML = "Vous avez bien meritez un <a href=\"cat.html\">chat.</a>";
    console.log(resArr);
 }
